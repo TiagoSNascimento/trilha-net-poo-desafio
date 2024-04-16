ee158177ddb797e67d0375144b26d9eef8d8bef0
TiagoSNascimento <jamesnascwhite@hotmail.com>
2024-04-16 10:46:15 -0300

    desafio de trilha realizado com sucesso

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "351912578176", modelo: "Modelo I1", imei: "2222222", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "351912581544", modelo: "Modelo N1", imei: "3333333", memoria: 64);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
